d9b1228067374ab90cbd6dd1356beaea5330c0f0
Anuraggupta4333 <anuraggangil@gmail.com>
2025-06-18 10:42:50 +0530

    Show reviews

--- frontend/src/app/views/reviews/routes.ts
+++ frontend/src/app/views/reviews/routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ReviewListComponent } from './review-list/review-list.component';
+// import { ReviewDetailComponent } from './review-details/review-detail.component'; // spelling fixed
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'reviews', pathMatch: 'full' },  
-  { path: 'reviews', component: ReviewListComponent },     
-  { path: '**', redirectTo: 'reviews' },                    
+  { path: '', redirectTo: 'reviews', pathMatch: 'full' },
+  { path: 'reviews', component: ReviewListComponent },
+//   { path: 'reviews/:id', component: ReviewDetailComponent }, // Added detail route
+  { path: '**', redirectTo: 'reviews' }
 ];
